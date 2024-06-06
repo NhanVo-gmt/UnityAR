8e28fba1f5d71c28796d6d61540cf684e74ff91f
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-06-06 14:09:28 +0700

    placemenet indicator

---
+++ Assets/Scripts/PlaceObjectOnPlane.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlaceObjectOnPlane : MonoBehaviour
+{
+    public GameObject placementIndicator;
+    private Pose placementPose;
+    private Transform placementTransform;
+    private bool placementPoseIsValid = false;
+    private TrackableId placedPlaneId = TrackableId.invalidId;
+
+    private ARRaycastManager m_RaycastManager;
+    private ARPlaneManager m_PlaneManager;
+    private static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+        m_PlaneManager = GetComponent<ARPlaneManager>();
+    }
+
+    private void Update()
+    {
+        UpdatePlacementPosition();
+        UpdatePlacementIndicator();
+    }
+
+    private void UpdatePlacementPosition()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        if (m_RaycastManager.Raycast(screenCenter, s_Hits, TrackableType.PlaneWithinPolygon))
+        {
+            placementPoseIsValid = s_Hits.Count > 0;
+            if (placementPoseIsValid)
+            {
+                placementPose = s_Hits[0].pose;
+                placedPlaneId = s_Hits[0].trackableId;
+
+                ARPlane arPlane = m_PlaneManager.GetPlane(placedPlaneId);
+                placementTransform = arPlane.transform;
+            }
+        }
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+}
